f693b959b86b751773ce6e428a130f1a42b0b013
Shafikovamaria <75277903+Shafikovamaria@users.noreply.github.com>
2020-12-25 03:34:02 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,40 +1,54 @@
 #include <stdio.h>
-int QuickSort ( int A[], int from, int to, int *a, int *b )
-{
- int x, i, j, temp;
- if ( from >= to ) return;
- i = from;
- j = to;
- x = A[(from+to)/2];
- while ( i < j ) {
- while ( A[i] < x ) {i ++;(* a)++;}
- while ( A[j] > x ) {j --;(*a)++;}
- if ( i <= j ) {
-if (i!=j)(*b)++;
- temp = A[i]; A[i] = A[j]; A[j] = temp;
- i ++;
- j --;
 
+void QuickSort(int A[], int from, int to, int *a, int *b) {
+    int x, i, j, temp;
+    if (from >= to) return;
+    i = from;
+    j = to;
+    x = A[(from + to) / 2];
+    while (i < j) {
+        while (i != (from + to) / 2 )
+        { (*a)++;
+        printf("sravn: %d and %d\n", A[i], x);
+        if(A[i] < x)
+            i++;
+        else
+            break;
+        }
 
- }
- }
- QuickSort ( A, from, j, &(*a),&(*b));
- QuickSort ( A, i, to,&(*a),&(*b) );
-}
-const int N = 5;
-main()
-{
-int i, j, A[N], c;
-int srav,perest;
-srav=0;
-perest=0;
-    for ( i = 0; i < N; i ++ )
-    {
-        printf("A[%d]> ", i );
-        scanf ("%d", &A[i]);
+        while (j != (from + to) / 2) {
+            (*a)++; printf("sravn: %d and %d\n", A[j], x);
+            if(A[j] > x)
+                j--;
+            else
+                break;
+        }
+        if (i <= j) {
+            if (i != j)(*b)++, printf("perestav: %d and %d\n", A[i], A[j]);
+            temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+            i++;
+            j--;
+        }
     }
-    QuickSort ( A, 0, N-1, &srav, &perest );
-for ( i = 0; i < N; i ++ )
- printf("%d ", A[i]);
-  printf("srav-%d perest-%d ", srav, perest );
+    QuickSort(A, from, j, a, b);
+    QuickSort(A, i, to, a, b);
+}
+
+const int N = 5;
+
+main() {
+    int i, A[N];
+    int srav, perest;
+    srav = 0;
+    perest = 0;
+    for (i = 0; i < N; i++) {
+        printf("A[%d]> ", i);
+        scanf("%d", &A[i]);
+    }
+    QuickSort(A, 0, N - 1, &srav, &perest);
+    for (i = 0; i < N; i++)
+        printf("%d ", A[i]);
+    printf("srav-%d perest-%d ", srav, perest);
 }
